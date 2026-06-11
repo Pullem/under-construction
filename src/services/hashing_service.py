2b4b7c7d1da04b4dd878fe6db46e18d6b7a3bf90
Pullem <ga.messtek@gmail.com>
2026-06-11 17:45:40 +0200

    simple mvp

--- src/services/hashing_service.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-class HashingService:
-    def sha256_file(self, path, chunk_size=8192):
-        h = hashlib.sha256()
-        with open(path, "rb") as f:
-            for chunk in iter(lambda: f.read(chunk_size), b""):
-                h.update(chunk)
-        return h.hexdigest()
